41f19fd0aa1363017f13f7225a9add698495d765
Martin <marb61a@gmail.com>
2020-10-15 19:58:11 +0100

    Updated OpenAccount test and added dependencies and ran cli to create test videos

--- CypressPageObjectModel/cypress/integration/testcases/OpenAccountTest.spec.js
+++ CypressPageObjectModel/cypress/integration/testcases/OpenAccountTest.spec.js
@@ -4,7 +4,7 @@ import HomePageActions from '../../pageobjects/pageactions/HomePageActions';
 import AddCustomerPageActions from '../../pageobjects/pageactions/AddCustomerPageActions';
 import OpenAccountPageActions from '../../pageobjects/pageactions/OpenAccountPageActions';
 
-describe("", () => {
+describe("Open account for Customer", () => {
     const homePage = new HomePageActions();
     const addCust = new AddCustomerPageActions();
     const openAccount = new OpenAccountPageActions();
@@ -20,13 +20,13 @@ describe("", () => {
         homePage.navigateToURL();
     });
 
-    it("", () => {
+    it("Open account test", () => {
         homePage.loginAsBankManager();
         addCust.addCustomerDetails(data.firstname, data.lastname, data.postcode);
         addCust.validateAlert(data.alertmsg);
         openAccount.openaccount(data.customer, data.currency);
-        openAccount.validateAlert();
-        
+        openAccount.validateAlert(data.accountalert);
+
     });
 
 });
