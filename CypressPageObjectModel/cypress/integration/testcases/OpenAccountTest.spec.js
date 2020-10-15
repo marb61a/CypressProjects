b359d732527d95a9f912df4a4d0604abc8aedf23
Martin <marb61a@gmail.com>
2020-10-15 19:36:47 +0100

    Added initial open account test and updated open account actions

---
+++ CypressPageObjectModel/cypress/integration/testcases/OpenAccountTest.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+import HomePageActions from '../../pageobjects/pageactions/HomePageActions';
+import AddCustomerPageActions from '../../pageobjects/pageactions/AddCustomerPageActions';
+import OpenAccountPageActions from '../../pageobjects/pageactions/OpenAccountPageActions';
+
+describe("", () => {
+
+});
