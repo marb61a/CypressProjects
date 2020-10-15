16f16cfbc10b291881dab6c4155a873adbac8dde
Martin <marb61a@gmail.com>
2020-10-15 12:17:18 +0100

    Updated AddCustomerTest and added initial OpenAccountPageElements

--- CypressPageObjectModel/cypress/integration/testcases/AddCustomerTest.spec.js
+++ CypressPageObjectModel/cypress/integration/testcases/AddCustomerTest.spec.js
@@ -7,14 +7,21 @@ describe("Adding a new customer", () => {
     const homePage = new HomePageActions();
     const addCust = new AddCustomerPageActions();
 
+    before(() => {
+        cy.fixture('testdata')
+            .then((data) => {
+                globalThis.data = data;
+            });
+    });
+
     beforeEach(() => {
         homePage.navigateToURL();
     });
 
     it("Add Customer Details", () => {
         homePage.loginAsBankManager();
-        addCust.addCustomerDetails("Martin", "Byrne", "123456");
-        addCust.validateAlert("Customer added successfully");
+        addCust.addCustomerDetails(data.firstname, data.lastname, data.postcode);
+        addCust.validateAlert(data.alertmsg);
     });
 
 });
---
+++ CypressPageObjectModel/cypress/pageobjects/pageelements/OpenAccountPageElements.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+const or = require('../../locators.json');
+
+export default class OpenAccountPageElements{
+    openAccountBtn(){
+        return cy.get();
+    }
+
+    processBtn(){
+        return cy.get();
+    }
+}
