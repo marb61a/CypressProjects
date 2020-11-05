b0c5c743c57c44b9a160db40d499a53951d67d31
Martin <marb61a@gmail.com>
2020-11-05 12:20:18 +0000

    Updated notes file and added basic contact us file and removed example files

---
+++ CypressBasics/cypress/integration/contact-us.js
@@ -0,0 +1,9 @@
+describe("Test contact us form via WebdriverUni", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        // Cypress code
+    });
+
+    it("Should not be able to submit an unsuccessful submission via contact us form as all fields are required", () => {
+        // Cypress code
+    });
+});
