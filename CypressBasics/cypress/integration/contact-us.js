f46ae926f5944219ecca07245d7f9dff7c71289e
Martin <marb61a@gmail.com>
2020-11-16 20:02:11 +0000

    Updated contact us sample test and notes file

--- CypressBasics/cypress/integration/contact-us.js
+++ CypressBasics/cypress/integration/contact-us.js
@@ -1,6 +1,14 @@
+/// <reference types="Cypress" />
+
 describe("Test contact us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         // Cypress code
+        cy.visit("http://www.webdriveruniversity.com/");
+        // cy.get('#contact-us > .thumbnail').click();
+        cy.get('#contact-us').click({
+            force: true
+        });
+
     });
 
     it("Should not be able to submit an unsuccessful submission via contact us form as all fields are required", () => {
