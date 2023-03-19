bb3a81d50fc165811757f6fd307169f48c1adb04
Martin <marb61a@gmail.com>
2023-03-19 19:39:07 +0000

    Added Homepage_Steps step definition file

---
+++ cypress-cucumber/cypress/support/step_definitions/Homepage_Steps.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+import { Given, When }  from ("@badeball/cypress-cucumber-preprocessor");
+
+const url = "http://www.webdriveruniversity.com"
+
+Given(`I navigate to the webdriveruniversity hompeage`, () => {
+    cy.visit(url)
+});
+
+When(`I click on the contact us button`, () => {
+    // Removes an attribute from the DOM on a certain element
+    cy.get("#contact-us").invoke("removeAttr", "target");
+});
