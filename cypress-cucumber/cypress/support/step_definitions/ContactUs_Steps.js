e9f6f7684fbc76736346d7bf53b6df076f3e6d0c
Martin <marb61a@gmail.com>
2023-03-19 20:13:43 +0000

    Update feature file for unsuccesful login

--- cypress-cucumber/cypress/support/step_definitions/ContactUs_Steps.js
+++ cypress-cucumber/cypress/support/step_definitions/ContactUs_Steps.js
@@ -16,9 +16,17 @@ When(`I enter an email address`, () => {
 });
 
 When(`I type a comment`, () => {
-
+    cy.get('textarea[name="message"]').type("Hello World");
 });
 
 When(`I click on the submit button`, () => {
+    cy.get('[type="submit"]').click();
+});
 
+Then(`I should be presented with a successful contact us submission message`, () => {
+    cy.get('h1').should('have.text', 'Thank You for your Message!');
+});
+
+Then(`I should be presented with a unsuccessful contact us submission message`, () => {
+    cy.get('body').contains('Error: Invalid email address');
 });
